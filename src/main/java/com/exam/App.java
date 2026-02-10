28713e26cb56b7ba1b84bd9f57031ebcbd2199d5
manikumar-kundena11 <ragavendramanikumar6@gmail.com>
2026-02-10 20:23:22 +0530

    Create App.java

---
+++ src/main/java/com/exam/App.java
@@ -0,0 +1,8 @@
+package com.exam;
+public class App {
+    public static void main(String[] args) {
+        System.out.println("-------------------------");
+        System.out.println("Build Successful!");
+        System.out.println("-------------------------");
+    }
+}
