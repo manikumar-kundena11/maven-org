fc20faf27610946c92a53544e577c67bacbe0b5b
manikumar-kundena11 <ragavendramanikumar6@gmail.com>
2026-02-10 20:38:22 +0530

    Update App.java

--- src/main/java/com/exam/App.java
+++ src/main/java/com/exam/App.java
@@ -2,7 +2,7 @@ package com.exam;
 public class App {
     public static void main(String[] args) {
         System.out.println("-------------------------");
-        System.out.println("Build Successful!");
+        System.out.println("Build Successful!-----Manikumar");
         System.out.println("-------------------------");
     }
 }
